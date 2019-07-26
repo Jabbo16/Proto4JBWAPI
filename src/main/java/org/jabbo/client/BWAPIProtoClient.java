f495e32108509f89914d3c719cc72c6f37ac782b
Jabbo16 <jabbo16@gmail.com>
2019-07-26 16:55:46 +0200

    tcp conn, trying to parse messages

--- src/main/java/org/jabbo/client/BWAPIProtoClient.java
+++ src/main/java/org/jabbo/client/BWAPIProtoClient.java
@@ -4,22 +4,32 @@ import bwapi.init.Init;
 import bwapi.message.MessageOuterClass;
 import org.apache.commons.math3.random.MersenneTwister;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
 import java.util.Deque;
 
 public class BWAPIProtoClient {
 
-    public BWAPIProtoClient(){
+    private SocketChannel tcpSocket; // blocking
+    private Deque<MessageOuterClass.Message> messageQueue;
+    private MersenneTwister mt;
+    private int connectionPort;
+
+    public BWAPIProtoClient() {
         this.mt = new MersenneTwister(System.nanoTime()); // Proper seed?
-        this.connected = false;
         this.connectionPort = 1025;
-        this.udpbound = false;
     }
 
     public void lookForServer(int apiVersion, String bwapiVersion, boolean tournament) throws IOException {
         if (isConnected()) return;
-        udpSocket = new DatagramSocket();
+        System.out.println("Looking for Server, apiVersion: " + apiVersion + ", bwapiVersion: " + bwapiVersion + ", tournamentMode: " + tournament);
+        // should be non blocking but its blocking, timeout should be set and handled
+        DatagramSocket udpSocket = new DatagramSocket();
         udpSocket.setBroadcast(true);
         Init.ClientBroadcast broadcast = Init.ClientBroadcast.newBuilder()
                 .setApiVersion(apiVersion)
@@ -29,63 +39,81 @@ public class BWAPIProtoClient {
         byte[] data = message.toByteArray();
         DatagramPacket packet
                 = new DatagramPacket(data, data.length, InetAddress.getByName("255.255.255.255"), 1024);
+        System.out.println("Sending UDP InitBroadcast message");
         udpSocket.send(packet);
         byte[] buffer = new byte[message.getSerializedSize()];
         DatagramPacket resp = new DatagramPacket(buffer, buffer.length);
         udpSocket.receive(resp);
         byte[] response = new byte[resp.getLength()];
         System.arraycopy(resp.getData(), 0, response, resp.getOffset(), resp.getLength());
-
-        MessageOuterClass.Message received = MessageOuterClass.Message.parseFrom(response);
-        System.out.println(received.toString());
+        MessageOuterClass.Message currentMessage = MessageOuterClass.Message.parseFrom(response);
+        if (!currentMessage.hasInitResponse()) {
+            udpSocket.close();
+            return;
+        }
+        System.out.println("Response from server\n" + currentMessage.toString());
+        connectionPort = currentMessage.getInitResponse().getPort();
+        System.out.println("Connecting to server using TCP socket and port " + connectionPort + "...");
+        tcpSocket = SocketChannel
+                .open(new InetSocketAddress("localhost", connectionPort));
+        //tcpSocket.connect(new InetSocketAddress("localhost", connectionPort));
+        if(!tcpSocket.isConnected()) System.out.println("Connection Failed");
+        System.out.println("Connected to server");
         udpSocket.close();
-        // TODO finish TCP conn
-
     }
+
 
     public void transmitMessages(){
-
+        if (!isConnected()) return;
     }
 
-    public void receiveMessages(){
+    public void receiveMessages() throws IOException {
+        if (!isConnected()) return;
+        while(true){
+            ByteBuffer buf = ByteBuffer.allocate(1000);
+            int numBytesRead = tcpSocket.read(buf);
 
+            System.out.println(numBytesRead);
+            if (numBytesRead <= 0) {
+                System.out.println("Finished reading stream");
+                return;
+            }
+            byte[] test = new byte[numBytesRead];
+            for(int ii=0; ii < numBytesRead; ii++){
+                test[ii] = buf.get(ii);
+            }
+            System.out.println(Arrays.toString(buf.array()));
+            System.out.println(Arrays.toString(test));
+            try{
+                MessageOuterClass.Message received = MessageOuterClass.Message.parseFrom(test);
+                System.out.println(received.toString());
+            } catch(com.google.protobuf.InvalidProtocolBufferException ignored){
+                System.out.println("Cant parse message to protobuf");
+            }
+
+        }
     }
 
-    public void disconnect(){
-
-    }
-
-    public void initListen(){
-
-    }
-
-    public void stopListen(){
-
+    public void disconnect() throws IOException {
+        if (!isConnected()) return;
+        tcpSocket.close();
     }
 
     public void queueMessage(MessageOuterClass.Message newMessage){
-
+        this.messageQueue.add(newMessage);
     }
 
     public MessageOuterClass.Message getNextMessage(){
-        return null;
+        return this.messageQueue.pop();
     }
 
     public final boolean isConnected(){
-        return false;
+        return this.tcpSocket != null && this.tcpSocket.isConnected();
     }
 
     public final int messageQueueSize(){
-        return 0;
+        return this.messageQueue.size();
     }
-
-    private DatagramSocket udpSocket; // should be non blocking
-    private Socket tcpSocket; // blocking
-    private Deque<MessageOuterClass.Message> messageQueue;
-    private MersenneTwister mt;
-    private boolean connected;
-    private boolean udpbound;
-    private short connectionPort;
 
     // TODO test?
     private int getRandomInteger(int min, int max){
--- src/main/java/org/jabbo/client/Main.java
+++ src/main/java/org/jabbo/client/Main.java
@@ -7,20 +7,22 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BWAPIProtoClient client = new BWAPIProtoClient();
-        while (true)
-        {
+        while (true) {
             System.out.println("Connecting...");
             client.lookForServer(0, "5.0", false);
             if (client.isConnected()) {
                 System.out.println("Connected");
-                return;
+                break;
             }
             try {
                 Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            return;
         }
+        while (true){
+            client.receiveMessages();
+        }
+
     }
 }
